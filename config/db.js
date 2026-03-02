ace9c42ef7c04f4876865e6ee0290168880560ad
cosmas255757 <csamwel363@gmail.com>
2026-03-02 04:04:28 +0300

    Updated DB to Neon cloud

--- config/db.js
+++ config/db.js
@@ -1,29 +1,11 @@
 import pkg from 'pg';
-import dotenv from 'dotenv';
-
-// Initialize environment variables
-dotenv.config();
-
 const { Pool } = pkg;
 
-// Use environment variables or fallback to defaults
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'loan',
-  //password: process.env.DB_PASSWORD, // Loaded from .env file
-  password: 'jose2002##',
-  port: process.env.DB_PORT || 5432,
-});
-
-// Optional: Log when the pool connects or hits an error
-pool.on('connect', () => {
-  console.log('🐘 PostgreSQL Pool connected');
-});
-
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle PostgreSQL client', err);
-  process.exit(-1);
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
